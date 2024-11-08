c9f9e15a89ff792c94b82bbde19ef3a83e73ccea
Antimekumawat111 <aktank810@gmail.com>
2024-11-08 15:09:19 +0530

    fix error

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,26 @@ import Gucci from "../public/images/Gucci.png";
 import Prada from "../public/images/Prada.png";
 import CalvinKlein from "../public/images/CalvinKlein.png";
 
-function Photo({ cardNo, id, imagesrc, pvalue, rate }: any) {
+type PhotoProps = {
+  cardNo: string;
+  id: string;
+  imagesrc: string;
+  pvalue: string;
+  rate: string;
+};
+
+function Photo({ cardNo, id, imagesrc, pvalue, rate }: PhotoProps) {
   return (
     <div className="cards" id={cardNo}>
       <div className="card">
-        <img src={imagesrc} alt="product" className="card_img" id={id} />
+        <Image
+          src={imagesrc}
+          alt="product"
+          className="card_img"
+          id={id}
+          width={300} // Adjust width as required
+          height={300} // Adjust height as required
+        />
         <div className="card_info">
           <a href="" target="_blank">
             <p id="ptitle">{pvalue}</p>
@@ -37,24 +52,26 @@ export default function Home() {
       <div className="page1">
         <Header />
         <SubHeader />
-        <Image src={Rectangle2} alt="poster" id="posterImg" />
+        <Image src={Rectangle2} alt="poster" id="posterImg" width={500} height={300} />
         <LeftContainer />
-        <Image src={Vector} alt="Vector" id="vector" />
-        <Image src={Vector2} alt="Vector" id="vector2" />
+        <Image src={Vector} alt="Vector" id="vector" width={100} height={100} />
+        <Image src={Vector2} alt="Vector" id="vector2" width={100} height={100} />
         <Btn id="shopBtn" />
       </div>
 
       <div className="page2">
         <div className="logos">
-          <Image src={Versace} alt="Versace" id="versaceImg" className="logo" />
-          <Image src={Zara} alt="Zara" id="ZaraImg" className="logo" />
-          <Image src={Gucci} alt="Gucci" id="GucciImg" className="logo" />
-          <Image src={Prada} alt="Prada" id="PradaImg" className="logo" />
+          <Image src={Versace} alt="Versace" id="versaceImg" className="logo" width={100} height={100} />
+          <Image src={Zara} alt="Zara" id="ZaraImg" className="logo" width={100} height={100} />
+          <Image src={Gucci} alt="Gucci" id="GucciImg" className="logo" width={100} height={100} />
+          <Image src={Prada} alt="Prada" id="PradaImg" className="logo" width={100} height={100} />
           <Image
             src={CalvinKlein}
             alt="Calvin Klein"
             id="CalvinKleinImg"
             className="logo"
+            width={100}
+            height={100}
           />
           <h1 id="newArrivals">NEW ARRIVALS</h1>
 
@@ -62,8 +79,6 @@ export default function Home() {
             cardNo="one"
             id="firshirtImg"
             imagesrc="/images/frame1.png"
-            title="First Image"
-            sname="first"
             pvalue="T-SHIRT WITH TAPE DETAILS"
             rate="120"
           />
@@ -72,8 +87,6 @@ export default function Home() {
             cardNo="second"
             id="secondshirtImg"
             imagesrc="/images/frame2.png"
-            title="First Image"
-            sname="first"
             pvalue="SKINNY FIT JEANS"
             rate="240"
           />
@@ -82,8 +95,6 @@ export default function Home() {
             cardNo="third"
             id="thirdshirtImg"
             imagesrc="/images/frame3.png"
-            title="First Image"
-            sname="first"
             pvalue="CHECKERED SHIRT"
             rate="180"
           />
@@ -91,8 +102,6 @@ export default function Home() {
             cardNo="fourth"
             id="fourthshirtImg"
             imagesrc="/images/frame4.png"
-            title="First Image"
-            sname="first"
             pvalue="SLEEVE STRIPED T-SHIRT"
             rate="130"
           />
